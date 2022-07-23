65677352efc97bcf5d498da4811f8a6d24041621
SLMT <sam123456777@gmail.com>
2022-07-23 11:02:30 +0800

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+
+use dotenv::dotenv;
+use ftx::{options::Options, rest::{Rest, GetFutures, Future, GetHistoricalPrices}};
+use chrono::prelude::*;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(name = "FTX Price Fetcher")]
+#[clap(author = "Yu-Shan Lin <sam123456777@gmail.com>")]
+#[clap(version = "1.0")]
+#[clap(about = "A tool to download price data from FTX.")]
+struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Top10,
+    Download {
+        market_name: String,
+        start_time: DateTime<Local>,
+        end_time: Option<DateTime<Local>>
+    }
+}
+
 fn main() {
+    // Read '.env' file
+    dotenv().ok();
+
+    // Read parameters
+    let args = Args::parse();
+
     println!("Hello, world!");
 
     // TODO: Get the parameters (symbol, start date, end date)
@@ -9,3 +41,5 @@ fn main() {
 
     // TODO: Save the results to a CSV file
 }
+
+

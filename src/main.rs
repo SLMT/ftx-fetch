f1cd5e8a17aa12aa4729ac172b7d01ee3ac14164
SLMT <sam123456777@gmail.com>
2022-07-23 22:47:32 +0800

    Implementing...

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::sync::mpsc::{channel, Receiver, Sender};
 
-use std::sync::Mutex;
-
-use dotenv::dotenv;
-use ftx::{options::Options, rest::{Rest, GetFutures, Future, GetHistoricalPrices, Candle}};
 use chrono::prelude::*;
 use clap::{Parser, Subcommand};
-use prettytable::{row, cell, Table};
+use dotenv::dotenv;
+use ftx::{
+    options::Options,
+    rest::{Candle, Future, GetFutures, GetHistoricalPrices, Rest},
+};
+use prettytable::{cell, row, Table};
 use rust_decimal_macros::dec;
+use tokio::time::Instant;
 
 type Ftx = Rest;
 
@@ -29,15 +32,18 @@ enum Commands {
     Download {
         #[clap(help = "name of the market")]
         market_name: String,
-        #[clap(help = "start time of the history")]
-        start_time: DateTime<Local>,
-        #[clap(help = "end time of the history. Default: now")]
-        end_time: Option<DateTime<Local>>,
-        #[clap(default_value = "15", help = "window length in seconds. \
+        #[clap(help = "start date of the history (YYYY-mm-dd)")]
+        start_date: String,
+        #[clap(help = "end date of the history (YYYY-mm-dd). Default: now")]
+        end_date: Option<String>,
+        #[clap(
+            default_value = "15",
+            help = "window length in seconds. \
             options: 15, 60, 300, 900, 3600, 14400, 86400, or \
-            any multiple of 86400 up to 30*86400")]
+            any multiple of 86400 up to 30*86400"
+        )]
         resolution: u32,
-    }
+    },
 }
 
 #[tokio::main]
@@ -53,17 +59,39 @@ async fn main() {
 
     match args.command {
         Commands::Tops { count } => tops(ftx, count).await,
-        Commands::Download { market_name, start_time, end_time, resolution } =>
-            unimplemented!("download is not implemented yet")
+        Commands::Download {
+            market_name,
+            start_date,
+            end_date,
+            resolution,
+        } => {
+            let start_time: DateTime<Utc> = parse_date(&start_date).and_hms(0, 0, 0).into();
+            let end_time: DateTime<Utc> = if let Some(end_date) = end_date {
+                parse_date(&end_date).and_hms(23, 59, 59).into()
+            } else {
+                Utc::now()
+            };
+
+            dbg!(start_time);
+            dbg!(end_time);
+        }
     }
+}
+
+fn parse_date(date_str: &str) -> Date<Local> {
+    let date = NaiveDate::parse_from_str(date_str, "%Y-%m-%d").unwrap();
+    Local.ymd(date.year(), date.month(), date.day())
 }
 
 async fn tops(ftx: Ftx, count: usize) {
     // Fetch futures and sort by volume (USD)
     let mut futures = ftx.request(GetFutures {}).await.unwrap();
-    futures.sort_by(|a, b|
-        a.volume_usd24h.unwrap().partial_cmp(&b.volume_usd24h.unwrap()).unwrap()
-    );
+    futures.sort_by(|a, b| {
+        a.volume_usd24h
+            .unwrap()
+            .partial_cmp(&b.volume_usd24h.unwrap())
+            .unwrap()
+    });
 
     // Create a pretty table
     let mut table = Table::new();
@@ -94,14 +122,57 @@ async fn tops(ftx: Ftx, count: usize) {
     table.printstd();
 }
 
-async fn download(ftx: Ftx, market_name: String, start_time: DateTime<Local>,
-        end_time: DateTime<Local>, resolution: u32) {
+async fn download(
+    ftx: Ftx,
+    market_name: String,
+    start_time: DateTime<Local>,
+    end_time: DateTime<Local>,
+    resolution: u32,
+) {
     // Create a shared data structure to store the results
-    // let history: Arc<Mutex<Vec<Candle>>> = Arc::new(Mutex::new(Vec::new()));
+    let (tx, rx): (Sender<Vec<Candle>>, Receiver<Vec<Candle>>) = channel();
 
     // TODO: Issue requests to fetch historical prices
+    let execute_time = Instant::now();
+    let task = tokio::time::timeout_at(
+        execute_time.clone(),
+        fetch_historical_price(
+            ftx,
+            market_name,
+            start_time,
+            end_time,
+            resolution,
+            execute_time,
+            tx,
+        ),
+    );
+    tokio::spawn(task);
 
     // TODO: Save the results to a CSV file
 
-    unimplemented!();
+    // unimplemented!();
+}
+
+async fn fetch_historical_price(
+    ftx: Ftx,
+    market_name: String,
+    start_time: DateTime<Local>,
+    end_time: DateTime<Local>,
+    resolution: u32,
+    execute_time: Instant,
+    out_channel: Sender<Vec<Candle>>,
+) {
+    let mut candles = ftx
+        .request(GetHistoricalPrices {
+            market_name,
+            resolution,
+            limit: None,
+            start_time: Some(start_time.with_timezone(&Utc)),
+            end_time: Some(end_time.with_timezone(&Utc)),
+        })
+        .await
+        .unwrap();
+
+    println!("{:?}", candles.first());
+    println!("{:?}", candles.last());
 }

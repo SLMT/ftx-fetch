631a3e9f7244454eb686aa7b51caa06b412059a8
SLMT <sam123456777@gmail.com>
2022-07-23 16:59:01 +0800

    Implement tops command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use dotenv::dotenv;
 use ftx::{options::Options, rest::{Rest, GetFutures, Future, GetHistoricalPrices}};
 use chrono::prelude::*;
 use clap::{Parser, Subcommand};
+use prettytable::{row, cell, Table};
+use rust_decimal_macros::dec;
 
 #[derive(Parser, Debug)]
 #[clap(name = "FTX Price Fetcher")]
@@ -16,7 +18,10 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Top10,
+    Tops {
+        #[clap(default_value = "10")]
+        count: usize,
+    },
     Download {
         market_name: String,
         start_time: DateTime<Local>,
@@ -24,22 +29,61 @@ enum Commands {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Read '.env' file
     dotenv().ok();
 
     // Read parameters
     let args = Args::parse();
 
-    println!("Hello, world!");
+    // Create a FTX connector
+    let ftx = Rest::new(Options::from_env());
 
-    // TODO: Get the parameters (symbol, start date, end date)
+    match args.command {
+        Commands::Tops { count } => tops(ftx, count).await,
+        Commands::Download { market_name, start_time, end_time } =>
+            unimplemented!("download is not implemented yet")
+    }
+}
 
+async fn tops(ftx: Rest, count: usize) {
+    let mut futures = ftx.request(GetFutures {}).await.unwrap();
+    futures.sort_by(|a, b|
+        a.volume_usd24h.unwrap().partial_cmp(&b.volume_usd24h.unwrap()).unwrap()
+    );
+
+    // Create a pretty table
+    let mut table = Table::new();
+    table.add_row(row![
+        "Rank",
+        "Market Name",
+        "Description",
+        "Volume in 24 HRs",
+        "Changes (last 1 HR)",
+        "Changes (last 24 HRs)",
+        "Changes (today)",
+    ]);
+    for (rank, future) in futures.iter().rev().take(count).enumerate() {
+        table.add_row(row![
+            format!("{}", rank + 1),
+            future.name,
+            future.description,
+            format!("{:.0} USD", future.volume_usd24h.unwrap()),
+            format!("{:.2}%", future.change1h.unwrap() * dec!(100)),
+            format!("{:.2}%", future.change24h.unwrap() * dec!(100)),
+            format!("{:.2}%", future.change_bod.unwrap() * dec!(100)),
+        ]);
+    }
+
+    // Print the result
+    table.printstd();
+}
+
+async fn download(ftx: Rest) {
     // TODO: Create a data structure to store the results
 
     // TODO: Issue requests to fetch historical prices
 
     // TODO: Save the results to a CSV file
 }
-
-

ed7bf05501bacab946c111193c55d67e6c4e2eff
SLMT <sam123456777@gmail.com>
2022-07-23 10:37:47 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    // TODO: Get the parameters (symbol, start date, end date)
+
+    // TODO: Create a data structure to store the results
+
+    // TODO: Issue requests to fetch historical prices
+
+    // TODO: Save the results to a CSV file
+}

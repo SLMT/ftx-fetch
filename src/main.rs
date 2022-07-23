75a3aee2d5282e036a90acc8a9bda65a4cae5b19
SLMT <sam123456777@gmail.com>
2022-07-23 18:01:45 +0800

    Start implementing download function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 
+use std::sync::Mutex;
+
 use dotenv::dotenv;
-use ftx::{options::Options, rest::{Rest, GetFutures, Future, GetHistoricalPrices}};
+use ftx::{options::Options, rest::{Rest, GetFutures, Future, GetHistoricalPrices, Candle}};
 use chrono::prelude::*;
 use clap::{Parser, Subcommand};
 use prettytable::{row, cell, Table};
 use rust_decimal_macros::dec;
+
+type Ftx = Rest;
 
 #[derive(Parser, Debug)]
 #[clap(name = "FTX Price Fetcher")]
@@ -23,9 +27,16 @@ enum Commands {
         count: usize,
     },
     Download {
+        #[clap(help = "name of the market")]
         market_name: String,
+        #[clap(help = "start time of the history")]
         start_time: DateTime<Local>,
-        end_time: Option<DateTime<Local>>
+        #[clap(help = "end time of the history. Default: now")]
+        end_time: Option<DateTime<Local>>,
+        #[clap(default_value = "15", help = "window length in seconds. \
+            options: 15, 60, 300, 900, 3600, 14400, 86400, or \
+            any multiple of 86400 up to 30*86400")]
+        resolution: u32,
     }
 }
 
@@ -38,16 +49,17 @@ async fn main() {
     let args = Args::parse();
 
     // Create a FTX connector
-    let ftx = Rest::new(Options::from_env());
+    let ftx = Ftx::new(Options::from_env());
 
     match args.command {
         Commands::Tops { count } => tops(ftx, count).await,
-        Commands::Download { market_name, start_time, end_time } =>
+        Commands::Download { market_name, start_time, end_time, resolution } =>
             unimplemented!("download is not implemented yet")
     }
 }
 
-async fn tops(ftx: Rest, count: usize) {
+async fn tops(ftx: Ftx, count: usize) {
+    // Fetch futures and sort by volume (USD)
     let mut futures = ftx.request(GetFutures {}).await.unwrap();
     futures.sort_by(|a, b|
         a.volume_usd24h.unwrap().partial_cmp(&b.volume_usd24h.unwrap()).unwrap()
@@ -64,6 +76,8 @@ async fn tops(ftx: Rest, count: usize) {
         "Changes (last 24 HRs)",
         "Changes (today)",
     ]);
+
+    // Iterate futures reversely
     for (rank, future) in futures.iter().rev().take(count).enumerate() {
         table.add_row(row![
             format!("{}", rank + 1),
@@ -80,10 +94,14 @@ async fn tops(ftx: Rest, count: usize) {
     table.printstd();
 }
 
-async fn download(ftx: Rest) {
-    // TODO: Create a data structure to store the results
+async fn download(ftx: Ftx, market_name: String, start_time: DateTime<Local>,
+        end_time: DateTime<Local>, resolution: u32) {
+    // Create a shared data structure to store the results
+    // let history: Arc<Mutex<Vec<Candle>>> = Arc::new(Mutex::new(Vec::new()));
 
     // TODO: Issue requests to fetch historical prices
 
     // TODO: Save the results to a CSV file
+
+    unimplemented!();
 }
